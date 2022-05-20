4f13784d8ebfc39d8009cbc6024d56e3951f454c
LAIBA KAMAL <laibakamal003@gmail.com>
2022-05-21 01:18:02 +0500

    ...

--- MC_ASSIGNMENT01_KidsEnglishBook/app/src/main/java/com/example/mc_assignment01_kidsenglishbook/MainActivity.java
+++ MC_ASSIGNMENT01_KidsEnglishBook/app/src/main/java/com/example/mc_assignment01_kidsenglishbook/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mc_assignment01_kidsenglishbook;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +16,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button goToRepoBtn=findViewById(R.id.goToRepo);
+        Button goToBookBtn=findViewById(R.id.goToBook);
+
         goToRepoBtn.setOnClickListener(view -> {
             Uri uri = Uri.parse("https://github.com/laibakamal/MC_ASSIGNMENT01_KidsEnglishBook"); // missing 'http://' will cause crashed
-            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-            startActivity(intent);
+            Intent intent1 = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent1);
+        });
+
+        goToBookBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2=new Intent(MainActivity.this,AtoZ.class);
+                startActivity(intent2);
+            }
         });
     }
 }
