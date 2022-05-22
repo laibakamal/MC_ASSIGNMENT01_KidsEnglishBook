3261b4fb3d8046a5932117d237aca8c58d918f2b
LAIBA KAMAL <laibakamal003@gmail.com>
2022-05-22 14:52:48 +0500

    ...

--- MC_ASSIGNMENT01_KidsEnglishBook/app/src/main/java/com/example/mc_assignment01_kidsenglishbook/AtoZ.java
+++ MC_ASSIGNMENT01_KidsEnglishBook/app/src/main/java/com/example/mc_assignment01_kidsenglishbook/AtoZ.java
@@ -26,51 +26,61 @@ public class AtoZ extends AppCompatActivity {
             {
                 intent.putExtra("alphabet","A");
                 startActivity(intent);
+                break;
             }
             case R.id.b:
             {
                 intent.putExtra("alphabet","B");
                 startActivity(intent);
+                break;
             }
             case R.id.c:
             {
                 intent.putExtra("alphabet","C");
                 startActivity(intent);
+                break;
             }
             case R.id.d:
             {
                 intent.putExtra("alphabet","D");
                 startActivity(intent);
+                break;
             }
             case R.id.e:
             {
                 intent.putExtra("alphabet","E");
                 startActivity(intent);
+                break;
             }
             case R.id.f:
             {
                 intent.putExtra("alphabet","F");
                 startActivity(intent);
+                break;
             }
             case R.id.g:
             {
                 intent.putExtra("alphabet","G");
                 startActivity(intent);
+                break;
             }
             case R.id.h:
             {
                 intent.putExtra("alphabet","H");
                 startActivity(intent);
+                break;
             }
             case R.id.i:
             {
                 intent.putExtra("alphabet","I");
                 startActivity(intent);
+                break;
             }
             case R.id.j:
             {
                 intent.putExtra("alphabet","J");
                 startActivity(intent);
+                break;
             }
             case R.id.k:
             {
@@ -81,76 +91,91 @@ public class AtoZ extends AppCompatActivity {
             {
                 intent.putExtra("alphabet","L");
                 startActivity(intent);
+                break;
             }
             case R.id.m:
             {
                 intent.putExtra("alphabet","M");
                 startActivity(intent);
+                break;
             }
             case R.id.n:
             {
                 intent.putExtra("alphabet","N");
                 startActivity(intent);
+                break;
             }
             case R.id.o:
             {
                 intent.putExtra("alphabet","O");
                 startActivity(intent);
+                break;
             }
             case R.id.p:
             {
                 intent.putExtra("alphabet","P");
                 startActivity(intent);
+                break;
             }
             case R.id.q:
             {
                 intent.putExtra("alphabet","Q");
                 startActivity(intent);
+                break;
             }
             case R.id.r:
             {
                 intent.putExtra("alphabet","R");
                 startActivity(intent);
+                break;
             }
             case R.id.s:
             {
                 intent.putExtra("alphabet","S");
                 startActivity(intent);
+                break;
             }
             case R.id.t:
             {
                 intent.putExtra("alphabet","T");
                 startActivity(intent);
+                break;
             }
             case R.id.u:
             {
                 intent.putExtra("alphabet","U");
                 startActivity(intent);
+                break;
             }
             case R.id.v:
             {
                 intent.putExtra("alphabet","V");
                 startActivity(intent);
+                break;
             }
             case R.id.w:
             {
                 intent.putExtra("alphabet","W");
                 startActivity(intent);
+                break;
             }
             case R.id.x:
             {
                 intent.putExtra("alphabet","X");
                 startActivity(intent);
+                break;
             }
             case R.id.y:
             {
                 intent.putExtra("alphabet","Y");
                 startActivity(intent);
+                break;
             }
             case R.id.z:
             {
                 intent.putExtra("alphabet","Z");
                 startActivity(intent);
+                break;
             }
 
         }
