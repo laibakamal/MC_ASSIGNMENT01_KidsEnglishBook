f31a27a8f1bb8dd616b7873dfb3422bb4ecb0712
LAIBA KAMAL <laibakamal003@gmail.com>
2022-05-22 23:52:14 +0500

    ...

---
+++ MC_ASSIGNMENT01_KidsEnglishBook/app/src/main/java/com/example/mc_assignment01_kidsenglishbook/ques3.java
@@ -0,0 +1,14 @@
+package com.example.mc_assignment01_kidsenglishbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ques3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ques3);
+    }
+}
